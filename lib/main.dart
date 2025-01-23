9344268166e67778d52a73dd6f8579cff9a83b7e
Malik Zuraiz <malikzuraiz1214@gmail.com>
2025-01-24 01:50:15 +0500

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
-    url: 'https://jrhcipgyfiygwirnfhcb.supabase.co',
+    url: ,
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyaGNpcGd5Zml5Z3dpcm5maGNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1ODE0MjEsImV4cCI6MjA1MzE1NzQyMX0.bcw7TAvKJhU9-zA_WlHtdTpgSv_ioKfcl3A1llWBB7g',
+        
   );
 
   Get.put(SupabaseService()); // Bind the SupabaseService
